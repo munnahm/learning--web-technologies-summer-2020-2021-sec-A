21d28dcd5496133e6327d354a6fbe29957cfa444
munnahm <munnagalaxya7@gmail.com>
2021-06-13 11:41:42 +0600

    PHP task7

---
+++ LAB_TASK_03_PHP_INTRO/7.php
@@ -0,0 +1,34 @@
+<table border="1" width="500px">
+	<tr>
+		<td>
+			<?php
+			for($i=1;$i<=3;$i++){
+				for($j=1;$j<=$i;$j++){
+					echo "*"." ";
+				}
+				echo "<br>";
+			}
+			?>
+		</td>
+		<td>
+			<?php
+			for($i=1;$i<=3;$i++){
+				for($j=1;$j<=4-$i;$j++){
+					echo $j." ";
+				}
+				echo "<br>";
+			}
+			?>
+		</td>
+		<td>
+			<?php
+			for($i=1;$i<=3;$i++){
+				for($j=1;$j<=4-$i;$j++){
+					echo chr($j+64)." ";
+				}
+				echo "<br>";
+			}
+			?>
+		</td>
+	</tr>
+</table>
