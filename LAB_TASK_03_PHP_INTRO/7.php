6b84355d4ed228d936b757331daa9a4f985f8bd4
munnahm <munnagalaxya7@gmail.com>
2021-06-13 12:41:08 +0600

    PHP task7 v2 updated

--- LAB_TASK_03_PHP_INTRO/7.php
+++ LAB_TASK_03_PHP_INTRO/7.php
@@ -23,8 +23,8 @@
 		<td>
 			<?php
 			for($i=1;$i<=3;$i++){
-				for($j=1;$j<=4-$i;$j++){
-					echo chr($j+64)." ";
+				for($j=1;$j<=$i;$j++){
+					echo chr($j+63+$i)." ";
 				}
 				echo "<br>";
 			}
