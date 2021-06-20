a2eecb65745ef52646f0d79c6c869653b9e492fe
munnahm <munnagalaxya7@gmail.com>
2021-06-20 23:01:36 +0600

    LAB TASK_5

---
+++ LAB_TASK_5_SESSION/profile.php
@@ -0,0 +1,69 @@
+
+<fieldset>
+							
+</fieldset>
+<?php
+	session_start();
+	if($_SESSION['flag']==1)
+	{
+?>
+<html>
+	<head></head>
+	<FORM method="POST">
+		<body>
+				<fieldset>
+				XCompany<h4 align="right">Logged in as <a href="profile.php" align="right"><?php echo $_SESSION['user']['name'];?></a>|
+				<a href="login.php">Logout</a>
+				</h4>
+					<table border=1 width=100%>
+					<tr>
+						<td>
+						<h4>Account<br>_________</h4>
+						<ul>
+							<li> <a href="loginDashbord.php" name="i1">Dashbord</a></li>
+							<li> <a href="profile.php">View Profile</a></li>
+							<li> <a href="editProfile.php">Edit Profile</a></li>
+							<li> <a href="changePicture.php">Change Profile Picture</a></li>
+							<li> <a href="changePassword.php">Change Password</a></li>
+							<li> <a href="login.php">Logout</a></li>
+						</ul>
+						</td>
+						<td>
+							<fieldset>
+								<legend>PROFILE</legend>
+								<table>
+									<tr>
+										<td>
+										Name: <?php echo $_SESSION['user']['name'];?><br>
+										Email: <?php echo $_SESSION['user']['email'];?><br>
+										Gender: <?php echo $_SESSION['user']['gender'];?><br>
+										Date of Birth: <?php echo $_SESSION['user']['dd'];?>|
+										<?php echo $_SESSION['user']['mm'];?>|
+										<?php echo $_SESSION['user']['yy'];?>
+										<br>
+										</td>
+										<td>
+										<img src="images.png"/>
+										</td>
+									</tr>
+								</table>
+								
+							</fieldset>
+						</td>
+					</tr>
+				
+					</table>
+					<center>
+					Copyright &copy 2017
+					</center>
+				</fieldset>
+		</body>
+	</FORM>
+</html>
+<?php
+	}
+	else
+		echo "Invalid Request!";
+?>
+<?php
+?>
