640e6de04f0d296e7c8950e7b1fd6008c94a50e4
munnahm <munnagalaxya7@gmail.com>
2021-06-20 10:42:00 +0600

    LAB TASK 5 B

---
+++ LAB_TASK_5_SESSION/registration.php
@@ -0,0 +1,84 @@
+<html>
+	<head></head>
+	<FORM method="POST" action="b.php">
+		<body>
+				<fieldset>
+				XCompany&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp
+				<a href="home.php" align="right">Home</a>|
+				<a href="login.php">Login</a>|
+				<a href="Registartion.php">Registartion</a>
+					<fieldset>
+					<fieldset>
+				<legend>REGISTRATION</LEGEND>
+					Name:
+					<input type="text" name="name" value=""><br>________________________<br>
+					Email
+					<input type="Email" name="name" value=""><br>________________________<br>
+					
+				
+				<!--3-->
+					Gender
+					
+						<input type="radio" name="gender" value="">Male
+						<input type="radio" name="gender" value="">Female
+						<input type="radio" name="gender" value="">Other<br>________________________<br>
+					
+					
+				
+				<!--4-->
+					Date of Birth
+					
+						<input type="number" name="dd" value="" size="100px">/
+						<input type="number" name="mm" value="" size="2px">/
+						<input type="number" name="yyyy" value="" size="4px">
+						(dd/mm/yyyy)
+						<br>________________________<br>
+					
+					
+				
+				<!--5-->
+					Blood Group
+					
+						<select>
+							<option name="">A+</option>
+							<option name="">B+</option>
+							<option name="">AB+</option>
+							<option name="">O+</option>
+							<option name="">A-</option>
+							<option name="">B-</option>
+							<option name="">AB-</option>
+							<option name="">O-</option>
+						</select><br>________________________<br>
+					
+					
+				
+				<!--6-->
+					Degree
+					
+						<input type="checkBox" name="Degree" value="">SSC
+						<input type="checkBox" name="Degree" value="">HSC
+						<input type="checkBox" name="Degree" value="">BSc
+						<input type="checkBox" name="Degree" value="">MSc<br>________________________<br>
+					
+					
+				
+				<!--7-->
+					Photo
+					Picture<input type="file" name="picture" value=""><br>________________________<br>
+				
+				<!--9-->
+						<input type="Submit" name="Submit">
+						<input type="reset" name="Reset"><br>
+					
+				
+			</fieldset>
+
+					
+					</fieldset>
+					<center>
+					Copyright &copy 2017
+					</center>
+				</fieldset>
+		</body>
+	</FORM>
+</html>
