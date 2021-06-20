fbd94aa009162e64721007005b40692e3ccdd20d
munnahm <munnagalaxya7@gmail.com>
2021-06-20 10:49:20 +0600

    LAB TASK 5 C

---
+++ LAB_TASK_5_SESSION/login.php
@@ -0,0 +1,41 @@
+<html>
+	<head></head>
+	<FORM method="POST" action="b.php">
+		<body>
+				<fieldset>
+				XCompany&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp
+				<a href="home.php" align="right">Home</a>|
+				<a href="login.php">Login</a>|
+				<a href="Registartion.php">Registartion</a>
+					<fieldset>
+					<fieldset>
+				<legend>LOGIN</LEGEND>
+					Name:
+					<input type="text" name="name" value=""><br>
+					Password:
+					<input type="password" name="password" value=""><br>________________________________________<br>
+					
+				
+					<!--6-->
+					
+						<input type="checkBox" name="RememmberMe" value="">Rememmber Me
+						<br>
+					
+					
+				
+						<!--9-->
+						<input type="Submit" name="Submit">
+						<br>
+					
+				
+			</fieldset>
+
+					
+					</fieldset>
+					<center>
+					Copyright &copy 2017
+					</center>
+				</fieldset>
+		</body>
+	</FORM>
+</html>
