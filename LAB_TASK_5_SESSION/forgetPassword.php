fa98da86a915c85ca30a15a51420d761e595aff3
munnahm <munnagalaxya7@gmail.com>
2021-06-20 22:59:59 +0600

    LAB TASK_5

---
+++ LAB_TASK_5_SESSION/forgetPassword.php
@@ -0,0 +1,33 @@
+<html>
+	<head></head>
+	<FORM method="POST" action="b.php">
+		<body>
+				<fieldset>
+				XCompany<h4 align="right"><a href="public_home.html" align="right">Home</a>|
+				<a href="login.php">Login</a>|
+				<a href="registration.php">Registartion</a>
+				</h4>
+					<fieldset>
+					<fieldset>
+				<legend>FORGET PASSWORD</LEGEND>
+					Enter Email:
+					<input type="email" name="Email" value="">
+					<br>___________________________________<br>
+					
+				
+						<!--9-->
+						<input type="Submit" name="Submit">
+						<br>
+					
+				
+			</fieldset>
+
+					
+					</fieldset>
+					<center>
+					Copyright &copy 2017
+					</center>
+				</fieldset>
+		</body>
+	</FORM>
+</html>
