a9cbb37b00d3394471f1002a58f6154f5bac0e61
munnahm <munnagalaxya7@gmail.com>
2021-08-08 10:49:23 +0600

    lab exam

--- WT_LAB_FINAL/delete.php
+++ WT_LAB_FINAL/delete.php
@@ -1,4 +1,22 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+
+</body>
+</html>
 <?php 
-
-
+		$conn=mysqli_connect('localhost','root','','webtech');
+		$sql = "DELETE FROM users WHERE id = '{$id}'";
+		$s="";
+		if( mysqli_query($conn, $sql))
+		{
+			$s.= "Data deleted...";
+		}
+		else
+		{
+			$s.="This data not found! ";
+		}
  ?>
--- WT_LAB_FINAL/search.php
+++ WT_LAB_FINAL/search.php
@@ -1,4 +1,19 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+
+</body>
+</html>
 <?php 
+	$conn=mysqli_connect('localhost','root','','webtech');
+	$sql = "select * from users";
+	$result = mysqli_query($conn, $sql);
+	$users = [];
 
-
+	while($user = mysqli_fetch_assoc($result)){
+		array_push($users, $user);
+	}
  ?>
